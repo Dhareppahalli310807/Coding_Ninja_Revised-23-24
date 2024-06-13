74e174fb7b12a2a0c1157ca6f6b5f4eb10934086
Dhareppa Halli <dharepps@gmail.com>
2024-06-13 13:03:34 +0530

    Changes For React Basic Week4 Reach Hooks added add to blog function

--- React-Basic/Week-4/React Hooks/Blogging App -Using Hooks/src/Components/Blog.js
+++ React-Basic/Week-4/React Hooks/Blogging App -Using Hooks/src/Components/Blog.js
@@ -1,35 +1,43 @@
 //Blogging App using Hooks
+import { useState } from "react";
+
 export default function Blog(){
-    
-    //Passing the synthetic event as argument to stop refreshing the page on submit
+
+    // const [title,setTitle] = useState("");
+    // const [content,setContent] = useState("");
+    const [formData, setformData] = useState({title:"", content:""})
+    const [blogs, setBlogs] =  useState([]);
+
     function handleSubmit(e){
         e.preventDefault();
+
+        setBlogs([{title: formData.title,content:formData.content}, ...blogs]);
+        console.log(blogs);
     }
 
     return(
         <>
-        {/* Heading of the page */}
         <h1>Write a Blog!</h1>
-
-        {/* Division created to provide styling of section to the form */}
         <div className="section">
 
         {/* Form for to write the blog */}
             <form onSubmit={handleSubmit}>
-
-                {/* Row component to create a row for first input field */}
                 <Row label="Title">
                         <input className="input"
-                                placeholder="Enter the Title of the Blog here.."/>
+                                placeholder="Enter the Title of the Blog here.."
+                                value={formData.title}
+                                onChange = {(e) => setformData({title: e.target.value, content:formData.content})}
+                        />
                 </Row >
 
-                {/* Row component to create a row for Text area field */}
                 <Row label="Content">
                         <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."/>
+                                placeholder="Content of the Blog goes here.."
+                                value={formData.content}
+                                onChange = {(e) => setformData({title: formData.title,content: e.target.value})}
+                        />
                 </Row >
-
-                {/* Button to submit the blog */}            
+         
                 <button className = "btn">ADD</button>
             </form>
                      
@@ -39,6 +47,13 @@ export default function Blog(){
 
         {/* Section where submitted blogs will be displayed */}
         <h2> Blogs </h2>
+        {blogs.map((blog,i) => (
+            <div className="blog">
+                <h3>{blog.title}</h3>
+                <hr/>
+                <p>{blog.content}</p>
+            </div>
+        ))}
         
         </>
         )
