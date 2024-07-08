2f2c14e44eaed255d5889e7320f237d98ead2f34
Dhareppa Halli <dharepps@gmail.com>
2024-07-08 17:33:27 +0530

    Changes For React Advanced Week3 React Toolkits Added ToolKits and extra reducers

--- React-Advanced/Week-3/To Do App Using Redux/src/components/ToDoList/ToDoList.js
+++ React-Advanced/Week-3/To Do App Using Redux/src/components/ToDoList/ToDoList.js
@@ -1,24 +1,25 @@
 import { useSelector, useDispatch } from "react-redux";
-import { toggleTodo } from "../../redux/actions/todoActions";
-
-import "./ToDoList.css";
+// import { toggleTodo } from "../../redux/actions/todoActions";
+import { actions } from "../../redux/reducers/todoReducer";
+import { todoSelector } from "../../redux/reducers/todoReducer";
+import styles from "./ToDoList.module.css";
 
 function ToDoList() {
 
-  const todos=useSelector((state)=> state.todoReducer.todos);
+  const todos=useSelector(todoSelector);
   console.log(todos);
   const disptach = useDispatch();
   // const todos= store.getState().todos;
 
   return (
-    <div className="container">
+    <div className={styles.container}>
     <ul>
       {todos.map((todo,index) => (
-        <li key={todo.id}>
-          <span className="content">{todo.text}</span>
-          <span className={todo.completed ? 'completed':'pending'}>{todo.completed ? 'Completed': 'Pending'}</span>
+        <li className={styles.item} key={todo.id}>
+          <span className={styles.content}>{todo.text}</span>
+          <span className={todo.completed ? styles.completed:styles.pending}>{todo.completed ? 'Completed': 'Pending'}</span>
           <button className="btn btn-warning"
-          onClick={()=>{disptach(toggleTodo(index))}}
+          onClick={()=>{disptach(actions.toggle(index))}}
           >Toggle</button>
           </li>
       ))}
